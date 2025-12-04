cb1786b14d8b77e081a4fd83d726537029858f39
Pasindu115530 <pasinduudana12m2@gmail.com>
2025-12-04 18:16:58 +0530

    Add Order model: implement orderSchema with fields for orderID, email, name, address, date, total, status, notes, and items

---
+++ Backend/models/Order.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema({
+    
+    orderID :{
+        type:String,
+        required:true,
+        unique: true
+    },
+    email:{
+        type:String,
+        required:true,
+
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true,
+        default: Date.now
+    },
+    total:{
+        type: Number,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true,
+        default: "Pending"
+    },
+    notes:{
+        type:String,
+        required:false
+    },
+    items:[
+        {
+            productID: {type:String, required:true},
+            name: {type:String, required:true},
+            price: {type:Number, required:true},
+            quantity: {type:Number, required:true},
+            image: {type:String, required:true}
+
+        }
+    ]
+
+
+
+})
